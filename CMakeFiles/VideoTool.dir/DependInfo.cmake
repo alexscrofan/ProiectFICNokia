82d9c5cb1b60a90a2797b1b476b69c98c9b75bb3
cloud <andreifulop1996@gmail.com>
2017-10-31 19:07:45 +0200

    racheta oti

--- CMakeFiles/VideoTool.dir/DependInfo.cmake
+++ CMakeFiles/VideoTool.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ SET(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/cloud/mm/fic/VideoTool.cpp" "/home/cloud/mm/fic/CMakeFiles/VideoTool.dir/VideoTool.cpp.o"
+  "/home/cloud/Desktop/racheta/fic/VideoTool.cpp" "/home/cloud/Desktop/racheta/fic/CMakeFiles/VideoTool.dir/VideoTool.cpp.o"
   )
 SET(CMAKE_CXX_COMPILER_ID "GNU")
 
